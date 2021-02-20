a41534841d96b6d8ad42fc44a236dc7522ca0c58
Co <645231528@qq.com>
2021-02-20 21:08:18 +0800

    提交

--- demeter-cloud-gateway/src/main/java/com/demeter/cloud/gateway/controller/GatewayActivityController.java
+++ demeter-cloud-gateway/src/main/java/com/demeter/cloud/gateway/controller/GatewayActivityController.java
@@ -1,7 +1,10 @@
 package com.demeter.cloud.gateway.controller;
 
 import com.demeter.cloud.model.persistence.controller.BaseController;
+import com.demeter.cloud.model.service.ActivityInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +20,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/gateway/activity")
 @Validated
 public class GatewayActivityController extends BaseController {
+    @Autowired
+    private ActivityInfoService activityInfoService;
+    @GetMapping("/activityInfoList")
+    public Object ActivityInfoList(){
+        logger.info("【请求开始】活动信息列表查询");
+        return activityInfoService.queryActivityInfoList();
+    }
 }
---
+++ demeter-cloud-model/src/main/java/com/demeter/cloud/model/service/impl/ActivityInfoServiceImpl.java
@@ -0,0 +1,108 @@
+package com.demeter.cloud.model.service.impl;
+
+import com.demeter.cloud.model.entity.ActivityInfo;
+import com.demeter.cloud.model.entity.ActivityInfoExample;
+import com.demeter.cloud.model.exception.BusinessException;
+import com.demeter.cloud.model.mapper.ActivityInfoMapper;
+import com.demeter.cloud.model.persistence.service.BaseService;
+import com.demeter.cloud.model.service.ActivityInfoService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * <p>封装Qicloud项目ActivityInfoServiceImp类.<br></p >
+ * <p>//TODO...<br></p >
+ *
+ * @author Powered by Administrator 2021-02-20 19:47
+ * @version 1.0.0
+ * <p>Copyright  2018-2021 Pivotal Cloud Technology Systems Incorporated. All rights reserved.<br></p >
+ */
+@Service("activityInfoService")
+@Transactional(rollbackFor = {BusinessException.class, RuntimeException.class, Exception.class})
+public class ActivityInfoServiceImpl extends BaseService implements ActivityInfoService {
+    @Resource
+    private ActivityInfoMapper activityInfoMapper;
+    /**
+     * 查询活动列表
+     *
+     * @return 返回列表
+     */
+    @Override
+    public List<ActivityInfo> queryActivityInfoList() {
+        ActivityInfoExample example = new ActivityInfoExample();
+        example.or().andIsDeleteEqualTo((byte)0).andStatusEqualTo((byte)1);
+        return activityInfoMapper.selectByExample(example);
+    }
+
+    /**
+     * 查询列表
+     *
+     * @param name  名称
+     * @param code
+     * @param page  页码
+     * @param limit 条数
+     * @param sort  排序
+     * @param order 排序
+     * @return 返回列表
+     */
+    @Override
+    public List<ActivityInfo> queryList(String name, String code, Integer page, Integer limit, String sort, String order) {
+        return null;
+    }
+
+    /**
+     * 查询
+     *
+     * @param id id
+     * @return 返回文件信息
+     */
+    @Override
+    public ActivityInfo queryById(Integer id) {
+        return null;
+    }
+
+    /**
+     * 更新
+     *
+     * @param activity 信息
+     * @return 返回文件信息
+     */
+    @Override
+    public int update(ActivityInfo activity) {
+        return 0;
+    }
+
+    /**
+     * 新增
+     *
+     * @param activity 文件信息
+     */
+    @Override
+    public void add(ActivityInfo activity) {
+
+    }
+
+    /**
+     * 查询
+     *
+     * @param code 文件索引
+     * @return
+     */
+    @Override
+    public ActivityInfo queryByCode(String code) {
+        return null;
+    }
+
+    /**
+     * 删除
+     *
+     * @param id 文件索引
+     */
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+}
